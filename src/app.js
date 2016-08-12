db1b0b1983d6e2f169597f09597ab0e6ab7a20cf
Banks, Martin <anotherbanksy@gmail.com>
2016-08-12 18:34:29 +1000

    add correctly positioned labels with # links and smoothscroll.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+(()=>{
 
 
 	const container = document.getElementById('appContainer');
@@ -5,15 +6,24 @@
 	var state={
 		lines: [
 			{
-				id: 'line1'
+				id: 'line1',
+				data: 'M54.7,14.9c-5.2,5.7-14.9,9.8-10.3,20.1s13.9,14.4,10.3,19.6S32,76.3,29.9,92.2c-0.7,5.1,0,14.1,1.4,24.3',
+				strokeColor: '#0f0'
 			},
 			{
-				id: 'line2'
+				id: 'line2',
+				data: 'M31.4,116.6c2.9,21.6,8.8,48.9,11.4,58.7c3.7,14.4,7.3,34,3.7,41.2s-24.7,59.8-24.7,74.7s0,40.2,0,40.2s0,13.4,3.1,17s8,4.1,13.8,11.3s9.4,3.6,2.7,15.5s-19.6,21.6-8.8,39.7',
+				strokeColor: '#00f'
 			},
 			{
-				id: 'line3'
+				id: 'line3',
+				data: 'M32.5,414.9c10.8,18,21.7,35.1,23,47.9s3.8,23.7,2.3,35.6s-18.6,30.4-13.9,39.7c4.6,9.3,10.8,28.4,9.3,33C51.6,575.8,50,585,50,585',
+				strokeColor: '#f00'
 			}
-		]
+
+		],
+		svgStyle: 'class="st0" fill="transparent" stroke-width="4" stroke-miterlimit="10" ',
+		labels: ''
 	}
 
 	function articleSectionTemplate(sectionId){
@@ -34,19 +44,33 @@
 		`
 	}
 
+
+	function linesTemplate(){
+		return state.lines.map((line, index)=>{
+			return `
+				<path id="${line.id}" ${state.svgStyle} stroke="${line.strokeColor}" d="${line.data}"/>
+			`
+		}).join('')
+		
+	}
+
+	
 	function mapTemplate(){
 		return `
 			<div class="svgHolder">
-				<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1280 1000" style="enable-background:new 0 0 1280 800;" xml:space="preserve">
-					<style type="text/css">
-						.st0{fill:none;}
-					</style>
+				<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 					<g class="all">
-						<path id="line1" class="st0" fill="transparent" stroke="#f00" stroke-width="4" stroke-miterlimit="10" d="M54.7,14.9c-5.2,5.7-14.9,9.8-10.3,20.1s13.9,14.4,10.3,19.6S32,76.3,29.9,92.2c-0.7,5.1,0,14.1,1.4,24.3"/>
-						<path id="line2" class="st0" fill="transparent" stroke="#0f0" stroke-width="4" stroke-miterlimit="10" d="M31.4,116.6c2.9,21.6,8.8,48.9,11.4,58.7c3.7,14.4,7.3,34,3.7,41.2s-24.7,59.8-24.7,74.7s0,40.2,0,40.2s0,13.4,3.1,17s8,4.1,13.8,11.3s9.4,3.6,2.7,15.5s-19.6,21.6-8.8,39.7"/>
-						<path id="line3" class="st0" fill="transparent" stroke="#00f" stroke-width="4" stroke-miterlimit="10" d="M32.5,414.9c10.8,18,21.7,35.1,23,47.9s3.8,23.7,2.3,35.6s-18.6,30.4-13.9,39.7c4.6,9.3,10.8,28.4,9.3,33C51.6,575.8,50,585,50,585"/>
+						${linesTemplate()}
 					</g>
 				</svg>
+				<div id="labelContainer">
+					<a id="sectionLabel0" href="#section1"><h4>before first label</h4></a>
+					<a id="sectionLabel1" href="#section2"><h4>End First label</h4></a>
+					<a id="sectionLabel2" href="#section3"><h4>End Second label</h4></a>
+					<a id="sectionLabel3" href="#section4"><h4>End Third label</h4></a>
+					
+				</div>
+				
 			</div>
 		`
 	}
@@ -60,28 +84,26 @@
 		var content = [
 			mapTemplate()
 		];
-		for(let i=0; i<parCount; i++){
-			content.push(articleSectionTemplate(i+1))
+		for(let i=0; i<=parCount; i++){
+			if (i<parCount){
+				content.push(articleSectionTemplate(i+1))
+			} else if (i === parCount){
+				let bigBox = window.innerHeight * 2
+				content.push(`<section id="section${i+1}"></section><div style="height:${bigBox}px; width: 100%; background:rgba(0,0,0,0.05)">spacer</div>`)
+			}	
 		}
-		let bigBox = window.innerHeight * 2
-		content.push(`<div style="height:${bigBox}px; width: 100%; background:lightgrey">spacer</div>`)
 		return content.join('')	
 	}
 
 
 
-
 	
-
-	function sectionPosition(elem){
-		return document.querySelector(elem).offsetTop - document.body.scrollTop
-	}
-
-	
-
 
 
 	function findHeights(){
+		state.contentHeight = document.getElementById('section1').clientHeight + 
+							document.getElementById('section2').clientHeight + 
+							document.getElementById('section3').clientHeight;
 		state.window = {
 			height: window.innerHeight
 		}
@@ -92,42 +114,44 @@
 
 	function findPositions(){
 		state.currentPos = document.body.scrollTop;
-		for (let i=0; i<parCount; i++){
-			let section = `section${i+1}`
-			let elem = document.getElementById(section)
-			state[section] = {
-				'heightPc': (elem.clientHeight / state.contentHeight) * 100,
-				'offset': elem.offsetTop - state.window.height
+		for (let i=0; i<=parCount; i++){
+			if(i<parCount){
+				let section = `section${i+1}`
+				let elem = document.getElementById(section)
+				state[section] = {
+					'heightPc': (elem.clientHeight / state.contentHeight) * 100,
+					'offset': elem.offsetTop - state.window.height
+				}
+
+				let startThis = document.getElementById(`section${i+1}`).offsetTop
+				state.lines[i].startPct = (startThis / state.pageHeight) * 100
+				state.lines[i].endPct = ( (startThis + elem.clientHeight) / state.pageHeight ) * 100 //100
+				
+				//document.querySelector(`#labelContainer a:nth-of-type(${i+1})`).style.top = `${(startThis / state.contentHeight) * 100}%`
+
+				if (i===0){
+					// first - section1
+					state.firstSection = section
+				} else if((i+1)===parCount){
+					// last - section3
+					state.lastSection = section
+				} else {
+					// middle sections
+				}
+				
+			} else if (i===parCount) {
+				//document.querySelector(`#labelContainer a:nth-of-type(${i+1})`).style.top = `95%`
+
 			}
+
 			
-			if (i===0){
-				var startNext = document.getElementById(`section${i+2}`).offsetTop;
-				var startThis = document.getElementById(`section${i+1}`).offsetTop;
-				// first - section1
-				state.firstSection = section
-				state.lines[i].startPct = ( (startThis) / state.pageHeight ) * 100
-				state.lines[i].endPct = ( (startNext) / state.pageHeight ) * 100
-				//return
-			} else if((i+1)===parCount){
-				var startThis = document.getElementById(`section${i+1}`).offsetTop;
-				// last - section3
-				state.lastSection = section
-				state.lines[i].startPct = ( (startThis) / state.pageHeight) * 100
-				state.lines[i].endPct = ((startThis + document.getElementById(section).clientHeight) / state.pageHeight) * 100 //100
-				//return 
-			} else {
-				var startThis = document.getElementById(`section${i+1}`).offsetTop
-				var startNext = document.getElementById(`section${i+2}`).offsetTop;
-				// middle - section2
-				state.lines[i].startPct = ((startThis) / state.pageHeight) * 100
-				state.lines[i].endPct = ( (startNext) / state.pageHeight) * 100
-				//return
-			}
-		};
+		}
 		let lastSection = document.getElementById(state.lastSection)
-		state.scrollPercent = ( (state.currentPos) / (state.pageHeight - lastSection.clientHeight - window.innerHeight ) ) * 100
-		state.windowPercent = ( document.body.scrollTop / (document.body.clientHeight - window.innerHeight) ) * 100
-		console.log('window percent:', state.windowPercent)
+			state.scrollPercent = ( (state.currentPos) / (state.pageHeight - lastSection.clientHeight - window.innerHeight ) ) * 100
+			state.windowPercent = ( document.body.scrollTop / (document.body.clientHeight - window.innerHeight) ) * 100
+			//console.log('window percent:', state.windowPercent)
+
+			
 	}
 
 
@@ -137,12 +161,40 @@
 	function renderPage(content, into){
 		into.innerHTML = content;
 		findHeights();
-		findPositions()
+		findPositions();
 	}
 	renderPage( pageTemplate(), appContainer )
 
 
 
+	function setProgressLabels() {
+		let currentPositon = document.body.scrollTop 
+		let progressPercent = (currentPositon / state.contentHeight) * 100
+		if( progressPercent >= 0 ){
+			document.querySelector('#labelContainer a:nth-of-type(1) h4').style.color = 'red'
+			document.querySelector('#labelContainer a:nth-of-type(2) h4').style.color = ''
+			document.querySelector('#labelContainer a:nth-of-type(3) h4').style.color = ''
+			document.querySelector('#labelContainer a:nth-of-type(4) h4').style.color = ''
+		}
+		if( progressPercent >= state.lines[1].startPct ){
+			document.querySelector('#labelContainer a:nth-of-type(1) h4').style.color = 'red'
+			document.querySelector('#labelContainer a:nth-of-type(2) h4').style.color = 'red'
+			document.querySelector('#labelContainer a:nth-of-type(3) h4').style.color = ''
+			document.querySelector('#labelContainer a:nth-of-type(4) h4').style.color = ''
+		}
+		if( progressPercent >= state.lines[2].startPct ){
+			document.querySelector('#labelContainer a:nth-of-type(1) h4').style.color = 'red'
+			document.querySelector('#labelContainer a:nth-of-type(2) h4').style.color = 'red'
+			document.querySelector('#labelContainer a:nth-of-type(3) h4').style.color = 'red'
+			document.querySelector('#labelContainer a:nth-of-type(4) h4').style.color = ''
+		}
+		if( progressPercent >= state.lines[2].endPct ){
+			document.querySelector('#labelContainer a:nth-of-type(1) h4').style.color = 'red'
+			document.querySelector('#labelContainer a:nth-of-type(2) h4').style.color = 'red'
+			document.querySelector('#labelContainer a:nth-of-type(3) h4').style.color = 'red'
+			document.querySelector('#labelContainer a:nth-of-type(4) h4').style.color = 'red'
+		}
+	}
 
 
 
@@ -166,10 +218,30 @@
 	}
 
 
+	function positionLabels(){
+		var labelPos = 0
+		for (var i=0; i<=state.lines.length; i++){
+			if(i<state.lines.length){
+				var data = state.lines[i];
+				var elem = document.getElementById(data.id);
+				var rect = elem.getBoundingClientRect().height
+				var dashLen = calcPathLength(elem);
+				console.log('heights', i, rect)
+				
+				document.querySelector(`#labelContainer a#sectionLabel${i}`).style.top = `${labelPos}px`
+			} else {
+				document.querySelector(`#labelContainer a#sectionLabel${i}`).style.top = `${labelPos}px`
+			}
+			labelPos += rect
+			
+		}
+
+	}
+
 	function scrollEventHandler(){
 		// Calculate how far down the page the user is 
 		var percentOfScroll = (( document.body.scrollTop / (document.body.clientHeight ) ) ) * 100
-		console.log(percentOfScroll)
+		//console.log(percentOfScroll)
 
 		// For each element that is getting drawn...
 		for (var i=0; i<state.lines.length; i++){
@@ -178,6 +250,7 @@
 
 			// Get the length of this elements path
 			var dashLen = calcPathLength(elem);
+			//console.log('dashLen', i, dashLen)
 
 			// Calculate where the current scroll position falls relative to our path
 			var fractionThroughThisElem = (percentOfScroll - data.startPct) / (data.endPct - data.startPct);
@@ -197,18 +270,19 @@
 	findPositions();
 	scrollEventHandler();
 	console.log(state)
-	
-	window.onscroll = ()=>{
-		scrollEventHandler();	
-	}
+	setProgressLabels()
+	positionLabels()
 	
 
-window.addEventListener("scroll", function () {
-    scrollEventHandler();	
-}, false);
+	window.addEventListener("scroll", function () {
+		scrollEventHandler();
+		setProgressLabels()
+
+	}, false);
 
 
 
 
 
+})()
 
